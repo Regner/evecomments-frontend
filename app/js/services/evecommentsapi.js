e1d6aa37717b1e159e651144e7e10be4397bf47e
Regner Blok-Andersen <shadowdf@gmail.com>
2015-01-12 12:16:14 +0000

    Adding :comment_id template value to the commentv1 service.

--- app/js/services/evecommentsapi.js
+++ app/js/services/evecommentsapi.js
@@ -4,7 +4,7 @@
     function backendService($resource) {
         var service = {}
 
-        service.commentsv1 = $resource('/api/v1/comments/1');
+        service.commentsv1 = $resource('/api/v1/comments/1/:comment_id');
 
         return service
     }
